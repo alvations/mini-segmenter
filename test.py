be60763240ef2af1d0ee4ff269418fc5a5f4bfee
alvations <alvations@gmail.com>
2015-03-18 11:05:57 +0100

    deleted files outside.

--- test.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf8 -*-
-import minisegmenter as mini
-import sys; reload(sys); sys.setdefaultencoding('utf8')
-sentence = u"应有尽有的丰富选择定将为您的旅程增添无数的赏心乐事"
-print mini.segmenter(sentence.decode('utf8'))
